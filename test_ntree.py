376f865eaa515ead1fa7f0b189fbeda1a9e55160
ArachnidAbby <spidertyler1122@gmail.com>
2026-04-15 11:58:12 -0400

    full NTree functionality for easy manipulation of abritrary tree structures

---
+++ test_ntree.py
@@ -0,0 +1,103 @@
+from typing import Any
+
+from compilertoolkit.ntree import NTree
+
+
+class ModuleName:
+    """Refer to module name, used simply for matching. Basically a speculation on a module we hope exists"""
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def matches(self, name: object) -> bool:
+        """Match this node based on some input param. Useful for module name resolution"""
+        if not isinstance(name, (ModuleName, Module)):
+            return self.name == name
+        return self.name == name.name
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, ModuleName):
+            return self.name == other.name
+        return self is other
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Module:
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def matches(self, name: object) -> bool:
+        """Match this node based on some input param. Useful for module name resolution"""
+        if not isinstance(name, (Module, ModuleName)):
+            return self.name == name
+        return self.name == name.name
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, ModuleName):
+            return self.name == other.name
+        return self is other
+
+    def __str__(self) -> str:
+        return f"<MODULE {self.name}>"
+
+
+imports = NTree[Module, str](
+    identifier="base",
+    leaves=[
+        Module("main"),
+        Module("other_mod"),
+        NTree(
+            identifier="lib",
+            leaves=[
+                Module("math"),
+                Module("system"),
+                Module("err"),
+                NTree(
+                    identifier="ui", leaves=[Module("application"), Module("widgets")]
+                ),
+            ],
+        ),
+    ],
+)
+
+trying_to_import = NTree[ModuleName, str](
+    identifier="base", leaves=[ModuleName("other_mod")]
+)
+
+trying_to_import_2 = NTree[ModuleName, str](
+    identifier="base",
+    leaves=[NTree(identifier="lib", leaves=[ModuleName("math")])],
+)
+
+trying_to_import_partial = NTree[ModuleName, str](
+    identifier="base",
+    leaves=[
+        NTree(identifier="lib", leaves=[ModuleName("something_that_does_not_exist")])
+    ],
+)
+
+print(imports.overlaps(trying_to_import))  # should be true
+print(imports.overlaps(trying_to_import_2))  # should be true
+print(imports.overlaps(trying_to_import_partial))  # should be false
+print()
+print(imports & trying_to_import)
+print(imports & trying_to_import_2)
+print(imports & trying_to_import_partial)
+
+print()
+
+print(imports | trying_to_import)
+print(imports | trying_to_import_2)
+print(imports | trying_to_import_partial)
+
+
+print()
+
+lib_pkg = imports["lib"]
+print(lib_pkg)
+
+main_mod = imports[ModuleName("main")]
+print(main_mod)
